cadcd694f63fbe3f0f42d74b8da44320c8a7dd31
Markov654 <iliq_markov@abv.bg>
2024-09-10 00:41:43 +0300

    try to react router

---
+++ src/pages/ForUs.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Forus (){
+    return(<h1>mql</h1>);
+}
+
+export default Forus;
---
+++ src/pages/Layout.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Car (){
+    return(<h1>bau</h1>);
+}
+
+export default Car;
